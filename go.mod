634b5f718cf613cd1418ff176215c62fbd268659
chadcarlson <chad.carlson@platform.sh>
2020-06-16 13:43:54 -0400

    Update gomeili.

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/chadwcarlson/pshindex
 go 1.14
 
 require (
-	github.com/chadwcarlson/gomeili v0.0.2
+	github.com/chadwcarlson/gomeili v0.0.3
 	github.com/kr/text v0.2.0 // indirect
 	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
 	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f // indirect
--- indexer.go
+++ indexer.go
@@ -1,14 +1,13 @@
 package main
 
 import (
+	"github.com/chadwcarlson/gomeili/config"
 	"github.com/chadwcarlson/gomeili/index"
-	"github.com/chadwcarlson/gomeili/utils/config"
 )
 
 func main() {
 
 	var config config.ConfigSet
-	combinedIndex := index.Build(config.Load("config.yaml"))
-	combinedIndex.Write("output/pshindex.json")
+	index.Build(config.Load("config.yaml"), "output/combined.json")
 
 }
